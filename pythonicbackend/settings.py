abbb7a5b848bec404b6e5b0ba73a24023f5ac4ee
cerebrium <nicholas.m.shankland@gmail.com>
2020-08-04 17:22:49 -0700

    oops

--- pythonicbackend/settings.py
+++ pythonicbackend/settings.py
@@ -118,12 +118,12 @@ WSGI_APPLICATION = 'pythonicbackend.wsgi.application'
 # }
 
 # Deployment Database Configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [

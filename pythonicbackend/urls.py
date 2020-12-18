9fc49432b273a662dfd58b80913a9331d9e7b5df
cerebrium <nicholas.m.shankland@gmail.com>
2020-12-18 14:49:32 -0800

    changed views

--- pythonicbackend/urls.py
+++ pythonicbackend/urls.py
@@ -41,5 +41,4 @@ urlpatterns = [
     path('supportschedule/', views.ReturnSupportSort.as_view(), name='other other name'),
     path('driverimages/', views.ReturnDriverImage.as_view(), name='Image with id'),
     path('driverhistory/', views.DriverHistoryView.as_view(), name='drivers associated with van'),
-
 ]
